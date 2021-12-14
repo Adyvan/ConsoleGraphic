a66913c9f7dda218b2876fc63c9071bb8865a7b6
Vladimir Adushkevich <vladimir.adushkevich@gmail.com>
2021-12-14 23:31:18 +0300

    add render Sircle

--- ConsoleGraphic/Program.cs
+++ ConsoleGraphic/Program.cs
@@ -2,11 +2,66 @@
 
 namespace ConsoleGraphic
 {
-    class Program
+    static class Program
     {
+        const float SymbolAspectRatio = 11f / 24f;
+        const int Width = 80;
+        const int Height = 24;
+        static byte[] screen = new byte[Width * Height];
+        static char[] gradient = " .:-=+*#%@".ToCharArray();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConsoleKeyInfo key = default;
+
+            var thread = new System.Threading.Thread(()=> {
+                while (key.Key != ConsoleKey.Escape)
+                {
+                    key = Console.ReadKey(false);
+                }
+            });
+
+            thread.Start();
+            var aspect = SymbolAspectRatio * Width / Height;
+            Vec2 uv = default;
+            while (key.Key != ConsoleKey.Escape)
+            {
+                for (int l = 0; l < Height ; l++)
+                {
+                    for (int c = 0; c < Width; c++)
+                    {
+                        uv = new Vec2(l, c) / new Vec2(Height, Width) * 2f - new Vec2(1);
+                        float x = l * 1f / Height * 2f - 1f;
+                        float y = c * 1f / Width * 2f - 1f;
+                        uv.Y *= aspect;
+
+                        screen[l * Width + c] = (1f - uv.SqrLenght).ToByte();
+                    }
+                }
+                PrintScreen();
+            }
+
+            thread.Join();
+        }
+
+        private static void PrintScreen()
+        {
+            foreach (var p in screen)
+            {
+                Console.Write(GetPixelColor(p));
+            }
+        }
+        
+
+        private static char GetPixelColor(byte intensity)
+        {
+            var index = (int)MathF.Round((gradient.Length - 1f) * intensity / byte.MaxValue);
+            return gradient[index];
+        }
+
+        private static byte ToByte(this float f)
+        {
+            return (byte) Math.Clamp(f * byte.MaxValue, byte.MinValue, byte.MaxValue);
         }
     }
 }
--- ConsoleGraphic/Vec2.cs
+++ ConsoleGraphic/Vec2.cs
@@ -3,8 +3,70 @@ namespace ConsoleGraphic
 {
     public class Vec2
     {
-        public Vec2()
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public float SqrLenght => X * X + Y * Y;
+        public float Lenght => MathF.Sqrt(X * X + Y * Y);
+        public Vec2 Normalize => new Vec2(X, Y) / Lenght;
+
+        public Vec2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Vec2(float xy)
+        {
+            X = xy;
+            Y = xy;
+        }
+
+        public Vec2() : this(0)
         {
         }
+
+        public static Vec2 operator +(Vec2 a, Vec2 b) => new Vec2(a.X + b.X, a.Y + b.Y);
+        public static Vec2 operator -(Vec2 a, Vec2 b) => new Vec2(a.X - b.X, a.Y - b.Y);
+        public static Vec2 operator *(Vec2 a, Vec2 b) => new Vec2(a.X * b.X, a.Y * b.Y);
+        public static Vec2 operator /(Vec2 a, Vec2 b) => new Vec2(a.X / b.X, a.Y / b.Y);
+        public static Vec2 operator *(Vec2 a, float f) => new Vec2(a.X * f, a.Y * f);
+        public static Vec2 operator /(Vec2 a, float f) => new Vec2(a.X / f, a.Y / f);
+    }
+
+    public class Vec3
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+
+        public float SqrLenght => X * X + Y * Y + Z * Z;
+        public float Lenght => MathF.Sqrt(X * X + Y * Y + Z * Z);
+        public Vec3 Normalize => new Vec3(X,Y,Z) / Lenght;
+
+        public Vec3(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vec3(float xyz)
+        {
+            X = xyz;
+            Y = xyz;
+            Z = xyz;
+        }
+
+        public Vec3() : this(0)
+        {
+        }
+
+        public static Vec3 operator +(Vec3 a, Vec3 b) => new Vec3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        public static Vec3 operator -(Vec3 a, Vec3 b) => new Vec3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        public static Vec3 operator *(Vec3 a, Vec3 b) => new Vec3(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+        public static Vec3 operator /(Vec3 a, Vec3 b) => new Vec3(a.X / b.X, a.Y / b.Y, a.Z / b.Z);
+        public static Vec3 operator *(Vec3 a, float f) => new Vec3(a.X * f, a.Y * f, a.Z * f);
+        public static Vec3 operator /(Vec3 a, float f) => new Vec3(a.X / f, a.Y / f, a.Z / f);
     }
 }
